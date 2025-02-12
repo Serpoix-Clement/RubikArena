88496513714781744518ac207c1623dda06c4807
Allandubras <allandubras@gmail.com>
2025-02-12 18:11:18 +0100

    Suite Menu 3/?
    continuité du menu avec  l'ajout des credits et lien entre main et credits

--- Assets/RubikArena/Scripts/Menu/MenuManager.cs
+++ Assets/RubikArena/Scripts/Menu/MenuManager.cs
@@ -5,11 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public Camera Cam;
+    public GameObject BackGroundMain;
+    public GameObject BackGroundCredit;
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+
+
 
     //Fonction pour lancer la partie 
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+    //Fonction pour les Credits 
+    public void Credits()
+    {
+        //je désactive le Fond Du MenuPrincipale
+        //pour Opti
+        BackGroundMain.SetActive(false);
+
+        //J'active le Fond Du Credits
+        //Car de base désactiver pour l'opti
+        BackGroundCredit.SetActive(true);
+
+        //la camera tourne 
+        Cam.transform.rotation = Quaternion.Euler(0, 180, 0);
+
+
+        //Je désactive le gameobject contenant Tout L'UI Du Menu Principale
+        MainMenu.SetActive(false);
+
+        //J'active le gameobject contenant tout l'UI du Credits
+        //Car de base désactiver
+        CreditsMenu.SetActive(true);
+
+    }
+
+    public void ReturnMain()
+    {
+        BackGroundMain.SetActive(true );
+        BackGroundCredit.SetActive(false);
+        Cam.transform.rotation = Quaternion.Euler(0,0,0);
+        CreditsMenu.SetActive(false );
+        MainMenu.SetActive(true);
     }
 
     //Fonction pour quitter le jeux
